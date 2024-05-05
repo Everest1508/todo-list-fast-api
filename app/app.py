5368ba3d98172d148b8008e2ed95f8e6ea957ef5
Everest1508 <riteshmahale15@gmail.com>
2024-05-05 20:07:45 +0530

    cc

--- app/app.py
+++ app/app.py
@@ -3,12 +3,20 @@ from api.routes.todo import todo_router
 from api.routes.user import auth_router
 from tortoise.contrib.fastapi import register_tortoise
 import os
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 load_dotenv()
 
 app = FastAPI()
 app.include_router(todo_router)
 app.include_router(auth_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Authorization", "Content-Type"],
+)
 register_tortoise(
     app=app,
     db_url = os.getenv('DB_URL'),
